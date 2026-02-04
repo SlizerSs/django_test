bd18cd5c4a8c866412d626aa29adf396fa49a953
SlizerSs <zerelaw@bk.ru>
2026-02-04 12:12:04 +0300

    added routing and redirection

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World! Это мой первый проект на Django!")
+    return HttpResponse("<h2>Главная</h2>")
+
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+
+def contact(request):
+    return HttpResponseRedirect("/about")
+
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
+
+
+def products(request, productid=1):
+    category = request.GET.get("cat", "")
+    output = "<h2>Продукт N {0} Категория: {1}</h2>".format(productid, category)
+    return HttpResponse(output)
+
+
+def users(request, id, name):
+    id = request.GET .get("id", 1)
+    name = request.GET.get("name", "Максим")
+    output = "<h2>Пользователь</h2><hЗ>id: {0} Имя: {1}</hЗ >".format(id, name)
+    return HttpResponse(output)
